2706268708cece490e9ac34aa991fb60ca1c464c
Louwrens <louwrenskoltz@gmail.com>
2025-01-17 09:17:03 +0200

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo) {
+        // Debug.Log(collisionInfo.collider.name);
+
+        if (collisionInfo.collider.tag == "Obstacle") {
+            // Debug.Log("We hit an obstacle!");
+            movement.enabled = false;
+        }
+    }
+}
