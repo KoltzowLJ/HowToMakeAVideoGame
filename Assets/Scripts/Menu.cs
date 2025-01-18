7910a4f275ab75a3aed82338be0b6b26b5916264
Louwrens <louwrenskoltz@gmail.com>
2025-01-18 15:13:30 +0200

    Update Final Tutorial Assets
    Add changes since initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,11 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,5 +20,9 @@ public class PlayerMovement : MonoBehaviour
         if ( Input.GetKey("a")) {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
+
+        if (rb.position.y < -1) {
+            FindFirstObjectByType<GameManager>().GameOver();
+        }
     }
 }
